0f9c9d3306f265b8023a45cb03ea3e605ae2aa88
Luc0-0 <nipunsujesh28@gmail.com>
2025-12-06 23:15:00 +0530

    refactor: fix additional ESLint issues - split ThemeContext hook, remove unused refs/vars

---
+++ src/contexts/useTheme.js
@@ -0,0 +1,13 @@
+// src/contexts/useTheme.js
+// Custom hook for accessing theme context
+
+import { useContext } from 'react';
+import { ThemeContext } from './ThemeContext';
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error('useTheme must be used within a ThemeProvider');
+  }
+  return context;
+};
